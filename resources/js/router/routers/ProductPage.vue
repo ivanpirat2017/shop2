86b7092270c7e2059e19d8e916515e177db8cbfa
ivan <ivan6219621@gmail.com>
2023-06-20 18:45:08 +0300

    слайдер

--- resources/js/router/routers/ProductPage.vue
+++ resources/js/router/routers/ProductPage.vue
@@ -4,16 +4,11 @@
             <div class="ProductPageCart">
                 <div class="ProductPageCartTitle">
                     <div class="ProductPageCartTitleSlider">
-                        <carousel :items-to-show="1.5">
-                            <slide v-for="slide in  productData.imgs" :key="slide">
-                                <img :src="'/storage/' + slide.image" alt="" class="ProductPageCartTitleImg">
-                            </slide>
-
-                            <template #addons>
-                                <navigation />
-                                <pagination />
-                            </template>
-                        </carousel>
+                        <img :src="'/storage/' + img" alt="" class="ProductPageCartTitleSliderImg">
+                        <div class="ProductPageCartTitleImg">
+                            <img :src="'/storage/' + slide.image" alt="" @click="setImg(slide.image)"
+                                v-for="slide in  productData.imgs" :key="slide">
+                        </div>
                     </div>
 
                     <div>
@@ -22,7 +17,11 @@
                     </div>
 
                 </div>
+                <div class="ProductPageCartBuy">
+                    <button class="ProductPageCartBuyCart"><img src="../../../static/img/shopping-cart.png" alt="">
+                        купить</button>
 
+                </div>
 
 
             </div>
@@ -31,30 +30,34 @@
 </template>
 
 <script>
-import 'vue3-carousel/dist/carousel.css'
-import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
 export default {
-    components: {
-        Carousel,
-        Slide,
-        Pagination,
-        Navigation,
-    },
+
     mounted() {
         this.getProduct()
+
+
     },
     methods: {
         getProduct() {
             fetch("/api/product_item/" + this.$route.params.id).then(r => r.json()).then(r => {
                 if (r.data) {
                     this.productData = r.data[0]
+
+                    if (this.productData.imgs.length > 0) {
+
+                        this.img = this.productData.imgs[0].image
+                    }
                 }
             })
+        },
+        setImg(img) {
+            this.img = img
         }
     },
     data() {
         return {
-            productData: {}
+            productData: {},
+            img: ''
         }
     }
 }
@@ -73,18 +76,69 @@ export default {
         flex: 200px 1;
         background: white;
 
+        &Buy {
+            padding: 20px 0;
+
+            &Cart {
+                background: #05c855;
+
+            }
+
+            button {
+
+                height: 40px;
+                border-radius: 5px;
+                color: white;
+                display: flex;
+                gap: 10px;
+
+                img {
+                    width: 100%;
+                    height: 100%;
+                    object-fit: contain;
+                    filter: invert(1);
+                }
+            }
+        }
+
         &Title {
             display: flex;
-            gap: 50px;
-            flex-direction: column;
+
+            gap: 20px;
+
 
             &Slider {
-                height: 400px;
+                height: 510px;
+                width: 300px;
+                display: flex;
+                flex-direction: column;
+
+
+                &Img {
+                    height: 400px;
+                    border-radius: 10px;
+                    object-fit: contain;
+                    padding-bottom: 10px;
+                }
             }
 
             &Img {
-                height: 400px;
+                height: 100px;
+                width: 500px;
+                display: flex;
+                gap: 10px;
 
+                img {
+                    height: 100%;
+                    border-radius: 10px;
+                    object-fit: contain;
+                    opacity: 0.5;
+                    transition: 0.3s;
+
+                    &:hover {
+                        opacity: 1;
+                    }
+                }
             }
         }
     }
